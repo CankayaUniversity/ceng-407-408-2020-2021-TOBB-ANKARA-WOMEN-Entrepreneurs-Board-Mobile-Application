94612ec48a52fc508139781c5d0eda3b637e5341
iremozturk <iremozturk97@hotmail.com>
2021-03-23 22:33:16 +0300

    news, CV and advertisement classes added

--- backend/src/main/java/com/kgk/repository/admin/CVRepository.java
+++ backend/src/main/java/com/kgk/repository/admin/CVRepository.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 public interface CVRepository {
 
     Collection<CV> listAllCVs();
-    CV findCVById(int CVId);
+    CV findCVById(int cvId);
     void saveCV(CV cv);
     void updateCV(CV cv);
 
