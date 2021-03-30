9e56466f59310d2c0b4197b67809e7af3915bdd0
mertbayramusta <mertb_99@hotmail.com>
2021-03-30 17:18:10 +0300

    color, checkbox, and label fixed, occupation is now a dropbox that contains every sector in Turkey

--- frontend/src/app/pages/page-register/page-register.page.ts
+++ frontend/src/app/pages/page-register/page-register.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface Occupation {
+  name: string;
+}
+
 @Component({
   selector: 'app-page-register',
   templateUrl: './page-register.page.html',
@@ -8,8 +12,92 @@ import { Router } from '@angular/router';
 })
 export class PageRegisterPage implements OnInit {
 
-  public isToggled = false;
+  public isToggled   = false;
   constructor(private router: Router) { }
+  occupations: Occupation[] = [
+    {
+      name: 'Adalet ve Güvenlik',
+    },
+    {
+      name: 'Ağaç İşleri, Kağıt ve Kağıt Ürünleri',
+    },
+    {
+      name: 'Bilişim Teknolojileri',
+    },
+    {
+      name: 'Cam, Çimento ve Toprak',
+    },
+    {
+      name: 'Çevre',
+    },
+    {
+      name: 'Eğitim',
+    },
+    {
+      name: 'Elektrik ve Elektronik',
+    },
+    {
+      name: 'Enerji',
+    },
+    {
+      name: 'Finans',
+    },
+    {
+      name: 'Gıda',
+    },
+    {
+      name: 'İnşaat',
+    },
+    {
+      name: 'İş ve Yönetim',
+    },
+    {
+      name: 'Kimya, Petrol, Lastik ve Plastik',
+    },
+    {
+      name: 'Kültür, Sanat ve Tasarım',
+    },
+    {
+      name: 'Maden',
+    },
+    {
+      name: 'Medya, İletişim ve Yayıncılık',
+    },
+    {
+      name: 'Metal',
+    },
+    {
+      name: 'Otomotiv',
+    },
+    {
+      name: 'Sağlık ve Sosyal Hizmetler',
+    },
+    {
+      name: 'Spor ve Rekreasyon',
+    },
+    {
+      name: 'Tarım, Avcılık ve Balıkçılık',
+    },
+    {
+      name: 'Tekstil, Hazır Giyim, Deri',
+    },
+    {
+      name: 'Ticaret (Satış ve Pazarlama)',
+    },
+    {
+      name: 'Toplumsal ve Kişisel Hizmetler',
+    },
+    {
+      name: 'Turizm, Konaklama, Yiyecek-İçecek Hizmetleri',
+    },
+    {
+      name: 'Ulaştırma, Lojistik ve Haberleşme',
+    }
+  ];
+
+  compareWith(o1: Occupation, o2: Occupation) {
+    return o1 && o2 ? o1.name === o2.name : o1 === o2;
+  }
 
   ngOnInit() {
   }
