48e6c493fdb46f11047cece49ac6cb173fd3ce01
mertbayramusta <mertb_99@hotmail.com>
2021-03-31 11:28:20 +0300

    merged with home branch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -15,6 +15,14 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/page-home/page-home.module').then( m => m.PageHomePageModule)
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/page-login/page-login.module').then(m => m.PageLoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/page-register/page-register.module').then( m => m.PageRegisterPageModule)
+  },
 ];
 
 @NgModule({
