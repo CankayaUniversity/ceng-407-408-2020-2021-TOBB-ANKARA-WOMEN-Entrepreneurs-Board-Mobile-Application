9fe5a9e4dc90961d9a498dd6352e47f2b1b2928d
mertbayramusta <mertb_99@hotmail.com>
2021-03-31 13:43:04 +0300

    login page now has validation features and app-input changed with ion-input

--- frontend/src/app/pages/page-login/page-login.page.ts
+++ frontend/src/app/pages/page-login/page-login.page.ts
@@ -1,16 +1,46 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-page-login',
   templateUrl: './page-login.page.html',
   styleUrls: ['./page-login.page.scss'],
 })
 export class PageLoginPage implements OnInit {
-
-  constructor(private router: Router) { }
+  public isToggled   = false;
+  loginForm: FormGroup;
+  submitted =  false;
+  constructor(public formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = formBuilder.group({
+    username: [
+      '',
+      Validators.compose([
+        Validators.required
+      ])
+    ],
+    password: [
+      '',
+      Validators.compose([
+        Validators.required
+      ])
+    ],
+  }); }
 
   ngOnInit() {
+  }
+  get errorCtr() {
+    return this.loginForm.controls;
+  }
+  submitForm(formData: any) {
+    this.submitted = true;
+    if (!this.loginForm.valid) {
+      console.log('All fields are required.');
+      return false;
+    } else {
+      console.log(formData);
+    }
   }
 
   logMeIn(){
