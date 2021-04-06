aa976e69b3a590223585d5f5a00191846d43dea2
mertbayramusta <mertb_99@hotmail.com>
2021-04-06 16:39:40 +0300

    database action added, username changed with email, validations updated, and "women with laptop" image added to header

--- frontend/src/app/pages/page-login/page-login.page.ts
+++ frontend/src/app/pages/page-login/page-login.page.ts
@@ -2,6 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {LoginForm} from '../../providers/model/login-form.type';
+import {RegisterForm} from "../../providers/model/register-form.type";
 
 @Component({
   selector: 'app-page-login',
@@ -12,11 +16,12 @@ export class PageLoginPage implements OnInit {
   public isToggled   = false;
   loginForm: FormGroup;
   submitted =  false;
-  constructor(public formBuilder: FormBuilder, private router: Router) {
+  constructor(public formBuilder: FormBuilder, private router: Router, private http: HttpClient) {
     this.loginForm = formBuilder.group({
-    username: [
+    email: [
       '',
       Validators.compose([
+        Validators.pattern('[A-Za-z0-9._%+-]{2,}@[a-zA-Z-_.]{2,}[.]{1}[a-zA-Z]{2,}'),
         Validators.required
       ])
     ],
@@ -33,13 +38,22 @@ export class PageLoginPage implements OnInit {
   get errorCtr() {
     return this.loginForm.controls;
   }
-  submitForm(formData: any) {
+  async submitForm() {
+    const formData: LoginForm = this.loginForm.value;
     this.submitted = true;
-    if (!this.loginForm.valid) {
+    if (this.loginForm.invalid) {
       console.log('All fields are required.');
+      this.router.navigate(['/login']);
       return false;
     } else {
       console.log(formData);
+      try {
+        // For database action
+        const res = await this.http.post<LoginForm>(environment.apiUrl + '/api/login-form', formData).toPromise();
+        this.router.navigate(['/home']);
+      } catch (e) {
+        // error toast
+      }
     }
   }
 
