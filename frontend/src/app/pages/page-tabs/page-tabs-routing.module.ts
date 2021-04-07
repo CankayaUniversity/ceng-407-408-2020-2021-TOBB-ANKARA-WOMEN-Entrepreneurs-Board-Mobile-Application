8fd416ff856ba171ee429f3596888d784ab0477f
ranacakmak <rana.cakmak.89@gmail.com>
2021-04-07 21:11:42 +0300

    Tabs page is updated

---
+++ frontend/src/app/pages/page-tabs/page-tabs-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PageTabsPage } from './page-tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: PageTabsPage,
+    children: [
+      {
+        path: 'feed',
+        loadChildren: () => import('../page-feed/page-feed.module').then(m => m.PageFeedPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '../page-tabs/feed',
+        pathMatch: 'full'
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: '../page-tabs/feed',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PageTabsPageRoutingModule {}
