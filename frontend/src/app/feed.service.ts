fc5f6b533f3ace60241965a2abe7769b1eb13a68
ranacakmak <rana.cakmak.89@gmail.com>
2021-04-14 12:23:31 +0300

    Environment and Feed service are updated

--- frontend/src/app/feed.service.ts
+++ frontend/src/app/feed.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
-// const API_URL = environment.apiUrl;
-// const API_KEY = environment.apiKey;
+const API_URL = environment.apiUrl;
+const API_KEY = environment.apiKey;
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class FeedService {
   // currentArticle: any;
   constructor(private http: HttpClient) {}
 
-  /*getData(url) {
+  getData(url) {
     return this.http.get(`${API_URL}/${url}&apiKey=${API_KEY}`);
-  }*/
+  }
 }
