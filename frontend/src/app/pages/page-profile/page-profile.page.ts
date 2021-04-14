ed8b5f4e8ac8f439fdaa8a00b32905d34b5dc45d
mertbayramusta <mertb_99@hotmail.com>
2021-04-14 13:27:07 +0300

    user model, description, birthdate, ion-card added

--- frontend/src/app/pages/page-profile/page-profile.page.ts
+++ frontend/src/app/pages/page-profile/page-profile.page.ts
@@ -12,18 +12,49 @@ import {User} from '../../providers/model/user/user.model';
 export class PageProfilePage implements OnInit {
 
   // users: User[];
+  user: User[] = []; //
+  users = {userId: '',
+    city: '' ,
+    roleId: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    tobbRegisterId: '',
+    phone: '',
+    photo: '',
+    birthDate: '',  //
+    occupation: '',
+    description: '',
+    catalogList: ''};
 
-  constructor(private router: Router/*, private route: ActivatedRoute, private userApi: UserApi, private http: HttpClient*/) { }
+  constructor(private router: Router, private route: ActivatedRoute, /*private userApi: UserApi,*/ private http: HttpClient) { }
 
-  /*async ngOnInit() {
+  async ngOnInit() {
 
-    this.route.paramMap.subscribe(paramMap => {
+    /*this.route.paramMap.subscribe(paramMap => {
       const id = paramMap.get('id');
-    });
+    });*/
     const res = await this.http.get<User>(environment.apiUrl + '/api/{userId}').toPromise();
-    this.users = await this.userApi.getAll().toPromise();
-  }*/
-  ngOnInit(){}
+    this.route.paramMap.subscribe(paramMap => {
+      this.users.userId = paramMap.get(res.userId);
+      this.users.city = paramMap.get(res.city);
+      this.users.roleId = paramMap.get(res.roleId);
+      this.users.firstName = paramMap.get(res.firstName);
+      this.users.lastName = paramMap.get(res.lastName);
+      this.users.email = paramMap.get(res.email);
+      this.users.password = paramMap.get(res.password);
+      this.users.tobbRegisterId = paramMap.get(res.tobbRegisterId);
+      this.users.phone = paramMap.get(res.phone);
+      this.users.photo = paramMap.get(res.photo);
+      this.users.birthDate = paramMap.get(String(res.birthDate));
+      this.users.occupation = paramMap.get(res.occupation);
+      this.users.description = paramMap.get(res.description);
+      this.users.catalogList = paramMap.get(res.catalogList);
+    });
+    // this.users = await this.userApi.getAll().toPromise();
+  }
+  // ngOnInit(){}
   goBack(){
     this.router.navigate(['/login']);
   }
