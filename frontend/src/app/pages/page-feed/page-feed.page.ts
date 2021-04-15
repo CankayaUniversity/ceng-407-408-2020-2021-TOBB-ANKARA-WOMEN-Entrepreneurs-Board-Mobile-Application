03c828809925d5175d5475d14d7c7a1d5732ff31
ranacakmak <rana.cakmak.89@gmail.com>
2021-04-15 22:41:22 +0300

    Feed page is updated

--- frontend/src/app/pages/page-feed/page-feed.page.ts
+++ frontend/src/app/pages/page-feed/page-feed.page.ts
@@ -1,47 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedService } from 'src/app/feed.service';
 import { Router } from '@angular/router';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-page-feed',
   templateUrl: './page-feed.page.html',
   styleUrls: ['./page-feed.page.scss'],
 })
+
 export class PageFeedPage implements OnInit {
   data: any;
   page = 1;
   constructor(private feedService: FeedService, private router: Router) {}
 
   ngOnInit() {
-    /*this.feedService
-      .getData(
-        `everything?q=tesla&from=2021-03-03&sortBy=publishedAt&pageSize=5&page=${
-          this.page
-        }`
-      )
+    this.feedService.getFeed('/api/feed')
       .subscribe(data => {
         console.log(data);
         this.data = data;
-      });*/
+      });
   }
 
-  /*loadMoreNews(event) {
+  onGoToNewsSingle(news){
+    this.feedService.currentNews = news;
+    this.router.navigate(['/feed-single']);
+  }
+
+  loadMoreNews(event) {
     this.page++;
     console.log(event);
-    this.feedService
-      .getData(
-        `everything?q=tesla&from=2021-03-03&sortBy=publishedAt&pageSize=5&page=${
-          this.page
-        }`
-      )
+    this.feedService.getFeed(environment.apiUrl)
       .subscribe(data => {
         // console.log(data);
         // this.data = data;
-        for (const article of data['articles']) {
-          this.data.articles.push(article);
+        for (const news of 'feed') {
+          this.data.feed.push(news);
         }
         event.target.complete();
         console.log(this.data);
       });
-  }*/
+  }
 }
