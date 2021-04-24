335e39c334094f55e0ea31f3f8f1a18823886269
mertbayramusta <mertb_99@hotmail.com>
2021-04-24 15:43:33 +0300

    message simulation and settings added

--- frontend/src/app/pages/page-message/page-message.page.ts
+++ frontend/src/app/pages/page-message/page-message.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Router} from '@angular/router';
+import {IonContent} from '@ionic/angular';
 @Component({
   selector: 'app-page-message',
   templateUrl: './page-message.page.html',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageMessagePage implements OnInit {
 
-  constructor() { }
+  messages = [
+    {
+      username: 'Katy Perry',
+      createdAt: 1554090856000,
+      msg: 'Finally got back that smile :)'
+    },
+    {
+      username: 'Dua Lipa',
+      createdAt: 1554090856000,
+      msg: 'We created something phenomenal\n' + 'Don\'t you agree?',
+    },
+    {
+      username: 'Taylor Swift',
+      createdAt: 1554090856000,
+      msg: 'You need to calm down!'
+    },
+
+  ];
+
+  currentUser = 'Katy Perry';
+  newMsg = '';
+  @ViewChild(IonContent) content: IonContent;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  goBack(){
+    this.router.navigate(['/home']);
+  }
+  sendMessage(){
+    this.messages.push({
+        username: 'Katy Perry',
+        createdAt: new Date().getTime(),
+        msg: this.newMsg
+      });
+    this.newMsg = '';
+    setTimeout(() => {
+      this.content.scrollToBottom(200);
+    });
   }
 
 }
