54606e821903f1041dcea301b6edcc5ab926c659
mertbayramusta <mertb_99@hotmail.com>
2021-05-06 21:53:21 +0300

    city section is now a dropbox

--- frontend/src/app/pages/page-register/page-register.page.ts
+++ frontend/src/app/pages/page-register/page-register.page.ts
@@ -9,6 +9,9 @@ import {RegisterForm} from '../../providers/model/register-form.type';
 interface Occupation {
   name: string;
 }
+interface City {
+  name: string;
+}
 
 @Component({
   selector: 'app-page-register',
@@ -50,9 +53,6 @@ export class PageRegisterPage implements OnInit {
       city: [
         '',
         Validators.compose([
-          Validators.minLength(4),
-          Validators.maxLength(30),
-          Validators.pattern('[0-9a-z-A-Z-_]*'),
           Validators.required
         ])
       ],
@@ -87,6 +87,251 @@ export class PageRegisterPage implements OnInit {
     });
   }
 
+  cities: City[] = [
+    {
+      name: 'Adana',
+    },
+    {
+      name: 'Adıyaman',
+    },
+    {
+      name: 'Afyonkarahisar',
+    },
+    {
+      name: 'Ağrı',
+    },
+    {
+      name: 'Aksaray',
+    },
+    {
+      name: 'Amasya',
+    },
+    {
+      name: 'Ankara',
+    },
+    {
+      name: 'Antalya',
+    },
+    {
+      name: 'Ardahan',
+    },
+    {
+      name: 'Artvin',
+    },
+    {
+      name: 'Aydın',
+    },
+    {
+      name: 'Balıkesir',
+    },
+    {
+      name: 'Bartın',
+    },
+    {
+      name: 'Batman',
+    },
+    {
+      name: 'Bayburt',
+    },
+    {
+      name: 'Bilecik',
+    },
+    {
+      name: 'Bingöl',
+    },
+    {
+      name: 'Bitlis',
+    },
+    {
+      name: 'Bolu',
+    },
+    {
+      name: 'Burdur',
+    },
+    {
+      name: 'Bursa',
+    },
+    {
+      name: 'Çanakkale',
+    },
+    {
+      name: 'Çankırı',
+    },
+    {
+      name: 'Çorum',
+    },
+    {
+      name: 'Denizli',
+    },
+    {
+      name: 'Diyarbakır',
+    },
+    {
+      name: 'Düzce',
+    },
+    {
+      name: 'Edirne',
+    },
+    {
+      name: 'Elazığ',
+    },
+    {
+      name: 'Erzincan',
+    },
+    {
+      name: 'Erzurum',
+    },
+    {
+      name: 'Eskişehir',
+    },
+    {
+      name: 'Gaziantep',
+    },
+    {
+      name: 'Giresun',
+    },
+    {
+      name: 'Gümüşhane',
+    },
+    {
+      name: 'Hakkari',
+    },
+    {
+      name: 'Hatay',
+    },
+    {
+      name: 'Iğdır',
+    },
+    {
+      name: 'Isparta',
+    },
+    {
+      name: 'İstanbul',
+    },
+    {
+      name: 'İzmir',
+    },
+    {
+      name: 'Kahramanmaraş',
+    },
+    {
+      name: 'Karabük',
+    },
+    {
+      name: 'Karaman',
+    },
+    {
+      name: 'Kars',
+    },
+    {
+      name: 'Kastamonu',
+    },
+    {
+      name: 'Kayseri',
+    },
+    {
+      name: 'Kırıkkale',
+    },
+    {
+      name: 'Kırklareli',
+    },
+    {
+      name: 'Kırşehir',
+    },
+    {
+      name: 'Kilis',
+    },
+    {
+      name: 'Kocaeli',
+    },
+    {
+      name: 'Konya',
+    },
+    {
+      name: 'Kütahya',
+    },
+    {
+      name: 'Malatya',
+    },
+    {
+      name: 'Manisa',
+    },
+    {
+      name: 'Mardin',
+    },
+    {
+      name: 'Mersin',
+    },
+    {
+      name: 'Muğla',
+    },
+    {
+      name: 'Muş',
+    },
+    {
+      name: 'Nevşehir',
+    },
+    {
+      name: 'Niğde',
+    },
+    {
+      name: 'Ordu',
+    },
+    {
+      name: 'Osmaniye',
+    },
+    {
+      name: 'Rize',
+    },
+    {
+      name: 'Sakarya',
+    },
+    {
+      name: 'Samsun',
+    },
+    {
+      name: 'Siirt',
+    },
+    {
+      name: 'Sinop',
+    },
+    {
+      name: 'Sivas',
+    },
+    {
+      name: 'Şanlıurfa',
+    },
+    {
+      name: 'Şırnak',
+    },
+    {
+      name: 'Tekirdağ',
+    },
+    {
+      name: 'Tokat',
+    },
+    {
+      name: 'Trabzon',
+    },
+    {
+      name: 'Tunceli',
+    },
+    {
+      name: 'Uşak',
+    },
+    {
+      name: 'Van',
+    },
+    {
+      name: 'Yalova',
+    },
+    {
+      name: 'Yozgat',
+    },
+    {
+      name: 'Zonguldak',
+    }
+  ];
 
   occupations: Occupation[] = [
     {
@@ -176,6 +421,7 @@ export class PageRegisterPage implements OnInit {
   async submitForm() {
     const formData: RegisterForm = {...this.ionicForm.value};
     formData.occupation = (formData.occupation as any).name;
+    formData.city = (formData.city as any).name;
 
     this.submitted = true;
     if (this.ionicForm.invalid && this.isToggled) {
@@ -187,7 +433,7 @@ export class PageRegisterPage implements OnInit {
       try {
         // For database action
         const res = await this.http.post<RegisterForm>(environment.apiUrl + '/api/register-form', formData).toPromise();
-        this.router.navigate(['/home']);
+        this.router.navigate(['/login']);
       } catch (e) {
         // error toast
       }
