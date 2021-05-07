d0d631cdc23f0c81e047f2a1a87694c7c24f67fc
ranacakmak <rana.cakmak.89@gmail.com>
2021-05-07 22:33:29 +0300

    Calendar page and Tabs page are updated

--- frontend/src/app/pages/page-calendar/page-calendar.page.ts
+++ frontend/src/app/pages/page-calendar/page-calendar.page.ts
@@ -2,7 +2,7 @@ import {Component, Inject, LOCALE_ID, OnInit, ViewChild} from '@angular/core';
 import {CalendarComponent} from 'ionic2-calendar';
 import {AlertController, ModalController} from '@ionic/angular';
 import {formatDate} from '@angular/common';
-import {PageCalendarEventPage} from "../page-calendar-event/page-calendar-event.page";
+import {PageCalendarEventPage} from '../page-calendar-event/page-calendar-event.page';
 
 @Component({
   selector: 'app-page-calendar',
@@ -59,67 +59,6 @@ export class PageCalendarPage implements OnInit {
     alert.present();
   }
 
-  createRandomEvents() {
-    var events = [];
-    for (var i = 0; i < 50; i += 1) {
-      var date = new Date();
-      var eventType = Math.floor(Math.random() * 2);
-      var startDay = Math.floor(Math.random() * 90) - 45;
-      var endDay = Math.floor(Math.random() * 2) + startDay;
-      var startTime;
-      var endTime;
-      if (eventType === 0) {
-        startTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + startDay
-          )
-        );
-        if (endDay === startDay) {
-          endDay += 1;
-        }
-        endTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + endDay
-          )
-        );
-        events.push({
-          title: 'All Day - ' + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: true,
-        });
-      } else {
-        var startMinute = Math.floor(Math.random() * 24 * 60);
-        var endMinute = Math.floor(Math.random() * 180) + startMinute;
-        startTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + startDay,
-          0,
-          date.getMinutes() + startMinute
-        );
-        endTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + endDay,
-          0,
-          date.getMinutes() + endMinute
-        );
-        events.push({
-          title: 'Event - ' + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: false,
-        });
-      }
-    }
-    this.eventSource = events;
-  }
-
   async openCalModal() {
     const modal = await this.modalCtrl.create({
       component: PageCalendarEventPage,
@@ -153,9 +92,5 @@ export class PageCalendarPage implements OnInit {
         this.myCal.loadEvents();
       }
     });
-  }
-
-  removeEvents() {
-    this.eventSource = [];
   }
 }
