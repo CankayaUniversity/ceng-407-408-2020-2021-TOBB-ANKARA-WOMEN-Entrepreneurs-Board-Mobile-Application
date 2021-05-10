ef6c71dbb42844295fc9f24d2a805f8e224d8632
mertbayramusta <mertb_99@hotmail.com>
2021-05-10 19:46:21 +0300

    Security Interceptor created

---
+++ frontend/src/app/providers/interceptor/security.interceptor.ts
@@ -0,0 +1,24 @@
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {AuthService} from "../service/auth.service";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class SecurityInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const credential = this.authService.getCredential().value;
+    if (credential) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + credential.access_token,
+        },
+      });
+    }
+    return next.handle(req);
+  }
+
+}
