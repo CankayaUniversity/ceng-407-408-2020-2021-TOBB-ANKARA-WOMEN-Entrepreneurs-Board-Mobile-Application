8a18584a4d04275e9783659e3a05f5230484efca
mertbayramusta <mertb_99@hotmail.com>
2021-05-11 22:39:29 +0300

    added updateMyProfile()

--- frontend/src/app/providers/model/user/user.api.ts
+++ frontend/src/app/providers/model/user/user.api.ts
@@ -2,6 +2,7 @@ import {BaseApi} from '../../base/base.api';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {User} from './user.model';
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class UserApi extends BaseApi<User> {
@@ -16,4 +17,8 @@ export class UserApi extends BaseApi<User> {
     this.update(2, {});
     this.delete(2);*/
   }
+
+  /*updateMyProfile(user: User) {
+    return this.http.put<User>(environment.apiUrl + '/api/user/profile', user);
+  }*/
 }
