66ac44fdf2487ebf0acd33c22ec4c82d88a10a7a
mertbayramusta <mertb_99@hotmail.com>
2021-05-12 20:58:31 +0300

    necessary edit-profile functions and designs added

--- frontend/src/app/pages/page-edit-profile/page-edit-profile.module.ts
+++ frontend/src/app/pages/page-edit-profile/page-edit-profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { PageEditProfilePage } from './page-edit-profile.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    PageEditProfilePageRoutingModule
+    PageEditProfilePageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [PageEditProfilePage]
 })
--- frontend/src/app/pages/page-edit-profile/page-edit-profile.page.ts
+++ frontend/src/app/pages/page-edit-profile/page-edit-profile.page.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../providers/model/user/user.model';
-import {FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../../providers/service/auth.service';
+import {UserApi} from '../../providers/model/user/user.api';
+
+interface Occupation {
+  name: string;
+}
+interface City {
+  name: string;
+}
 
 @Component({
   selector: 'app-page-edit-profile',
@@ -14,16 +22,361 @@ export class PageEditProfilePage implements OnInit {
   user: User;
   form: FormGroup;
 
-  constructor(private router: Router, private authService: AuthService, /*private userApi: UserApi, private formBuilder: FormBuilder*/) { }
+  constructor(private router: Router, private authService: AuthService, private userApi: UserApi, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      city: [''],
+      phone: [''],
+      birthDate: [''],
+      occupation: [''],
+      description: [''],
+    });
+  }
+
+  cities: City[] = [
+    {
+      name: 'Adana',
+    },
+    {
+      name: 'Adıyaman',
+    },
+    {
+      name: 'Afyonkarahisar',
+    },
+    {
+      name: 'Ağrı',
+    },
+    {
+      name: 'Aksaray',
+    },
+    {
+      name: 'Amasya',
+    },
+    {
+      name: 'Ankara',
+    },
+    {
+      name: 'Antalya',
+    },
+    {
+      name: 'Ardahan',
+    },
+    {
+      name: 'Artvin',
+    },
+    {
+      name: 'Aydın',
+    },
+    {
+      name: 'Balıkesir',
+    },
+    {
+      name: 'Bartın',
+    },
+    {
+      name: 'Batman',
+    },
+    {
+      name: 'Bayburt',
+    },
+    {
+      name: 'Bilecik',
+    },
+    {
+      name: 'Bingöl',
+    },
+    {
+      name: 'Bitlis',
+    },
+    {
+      name: 'Bolu',
+    },
+    {
+      name: 'Burdur',
+    },
+    {
+      name: 'Bursa',
+    },
+    {
+      name: 'Çanakkale',
+    },
+    {
+      name: 'Çankırı',
+    },
+    {
+      name: 'Çorum',
+    },
+    {
+      name: 'Denizli',
+    },
+    {
+      name: 'Diyarbakır',
+    },
+    {
+      name: 'Düzce',
+    },
+    {
+      name: 'Edirne',
+    },
+    {
+      name: 'Elazığ',
+    },
+    {
+      name: 'Erzincan',
+    },
+    {
+      name: 'Erzurum',
+    },
+    {
+      name: 'Eskişehir',
+    },
+    {
+      name: 'Gaziantep',
+    },
+    {
+      name: 'Giresun',
+    },
+    {
+      name: 'Gümüşhane',
+    },
+    {
+      name: 'Hakkari',
+    },
+    {
+      name: 'Hatay',
+    },
+    {
+      name: 'Iğdır',
+    },
+    {
+      name: 'Isparta',
+    },
+    {
+      name: 'İstanbul',
+    },
+    {
+      name: 'İzmir',
+    },
+    {
+      name: 'Kahramanmaraş',
+    },
+    {
+      name: 'Karabük',
+    },
+    {
+      name: 'Karaman',
+    },
+    {
+      name: 'Kars',
+    },
+    {
+      name: 'Kastamonu',
+    },
+    {
+      name: 'Kayseri',
+    },
+    {
+      name: 'Kırıkkale',
+    },
+    {
+      name: 'Kırklareli',
+    },
+    {
+      name: 'Kırşehir',
+    },
+    {
+      name: 'Kilis',
+    },
+    {
+      name: 'Kocaeli',
+    },
+    {
+      name: 'Konya',
+    },
+    {
+      name: 'Kütahya',
+    },
+    {
+      name: 'Malatya',
+    },
+    {
+      name: 'Manisa',
+    },
+    {
+      name: 'Mardin',
+    },
+    {
+      name: 'Mersin',
+    },
+    {
+      name: 'Muğla',
+    },
+    {
+      name: 'Muş',
+    },
+    {
+      name: 'Nevşehir',
+    },
+    {
+      name: 'Niğde',
+    },
+    {
+      name: 'Ordu',
+    },
+    {
+      name: 'Osmaniye',
+    },
+    {
+      name: 'Rize',
+    },
+    {
+      name: 'Sakarya',
+    },
+    {
+      name: 'Samsun',
+    },
+    {
+      name: 'Siirt',
+    },
+    {
+      name: 'Sinop',
+    },
+    {
+      name: 'Sivas',
+    },
+    {
+      name: 'Şanlıurfa',
+    },
+    {
+      name: 'Şırnak',
+    },
+    {
+      name: 'Tekirdağ',
+    },
+    {
+      name: 'Tokat',
+    },
+    {
+      name: 'Trabzon',
+    },
+    {
+      name: 'Tunceli',
+    },
+    {
+      name: 'Uşak',
+    },
+    {
+      name: 'Van',
+    },
+    {
+      name: 'Yalova',
+    },
+    {
+      name: 'Yozgat',
+    },
+    {
+      name: 'Zonguldak',
+    }
+  ];
+
+  occupations: Occupation[] = [
+    {
+      name: 'Adalet ve Güvenlik',
+    },
+    {
+      name: 'Ağaç İşleri, Kağıt ve Kağıt Ürünleri',
+    },
+    {
+      name: 'Bilişim Teknolojileri',
+    },
+    {
+      name: 'Cam, Çimento ve Toprak',
+    },
+    {
+      name: 'Çevre',
+    },
+    {
+      name: 'Eğitim',
+    },
+    {
+      name: 'Elektrik ve Elektronik',
+    },
+    {
+      name: 'Enerji',
+    },
+    {
+      name: 'Finans',
+    },
+    {
+      name: 'Gıda',
+    },
+    {
+      name: 'İnşaat',
+    },
+    {
+      name: 'İş ve Yönetim',
+    },
+    {
+      name: 'Kimya, Petrol, Lastik ve Plastik',
+    },
+    {
+      name: 'Kültür, Sanat ve Tasarım',
+    },
+    {
+      name: 'Maden',
+    },
+    {
+      name: 'Medya, İletişim ve Yayıncılık',
+    },
+    {
+      name: 'Metal',
+    },
+    {
+      name: 'Otomotiv',
+    },
+    {
+      name: 'Sağlık ve Sosyal Hizmetler',
+    },
+    {
+      name: 'Spor ve Rekreasyon',
+    },
+    {
+      name: 'Tarım, Avcılık ve Balıkçılık',
+    },
+    {
+      name: 'Tekstil, Hazır Giyim, Deri',
+    },
+    {
+      name: 'Ticaret (Satış ve Pazarlama)',
+    },
+    {
+      name: 'Toplumsal ve Kişisel Hizmetler',
+    },
+    {
+      name: 'Turizm, Konaklama, Yiyecek-İçecek Hizmetleri',
+    },
+    {
+      name: 'Ulaştırma, Lojistik ve Haberleşme',
+    }
+  ];
 
   async ngOnInit() {
     this.user = this.authService.getUser().value;
 
     // EDIT
-    /*this.form = this.formBuilder.group({
+    this.form = this.formBuilder.group({
       firstName: [this.user.firstName, Validators.required],
-      lastName: [this.user.lastName]
-    });*/
+      lastName: [this.user.lastName],
+      email: [this.user.email],
+      city: [this.user.city],
+      tobbRegisterId: [this.user.tobbRegisterId],
+      phone: [this.user.tobbRegisterId],
+      birthDate: [this.user.birthDate],
+      occupation: [this.user.occupation],
+      description: [this.user.description],
+    });
 
   }
   // ngOnInit(){}
@@ -31,13 +384,19 @@ export class PageEditProfilePage implements OnInit {
     this.router.navigate(['/tabs/profile']);
   }
 
-
+  compareWith(o1: Occupation, o2: Occupation) {
+    return o1 && o2 ? o1.name === o2.name : o1 === o2;
+  }
   // EDIT
-  /*async save(){
+  async save(){
     const userForm = this.form.value;
+
+    /*const userForm: User = {...this.form.value};
+    userForm.occupation = (userForm.occupation as any).name;
+    userForm.city = (userForm.city as any).name;
+    userForm.birthDate = (userForm.birthDate as any).name;*/
     const user = await this.userApi.updateMyProfile(userForm).toPromise();
     this.authService.setUser(user);
-  }*/
-
-  //FORM OLUCAK
+    this.router.navigate(['/tabs/feed']);
+  }
 }
