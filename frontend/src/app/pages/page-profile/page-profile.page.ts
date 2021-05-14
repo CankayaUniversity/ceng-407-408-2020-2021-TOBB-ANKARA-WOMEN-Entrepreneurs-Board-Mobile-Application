7b2b11dd67acd850374a6509f28c88a0f216d2f7
mertbayramusta <mertb_99@hotmail.com>
2021-05-14 13:24:18 +0300

    log out button added

--- frontend/src/app/pages/page-profile/page-profile.page.ts
+++ frontend/src/app/pages/page-profile/page-profile.page.ts
@@ -17,15 +17,15 @@ export class PageProfilePage implements OnInit {
   user: User;
   form: FormGroup;
 
-  constructor(private router: Router, private authService: AuthService, /*private userApi: UserApi, private formBuilder: FormBuilder*/) { }
+  constructor(private router: Router, private authService: AuthService) { }
 
   async ngOnInit() {
     this.user = this.authService.getUser().value;
     this.user.birthDate = format(new Date(this.user.birthDate), 'yyyy-MM-dd');
   }
   // ngOnInit(){}
-  goBack(){
-    this.router.navigate(['/feed']);
+  logout(){
+    this.router.navigate(['/login']);
   }
 
 }
