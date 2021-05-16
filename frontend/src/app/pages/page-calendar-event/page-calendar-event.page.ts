e6dc8cdd564cfc8b398a4865fd22c131e3902612
ranacakmak <rana.cakmak.89@gmail.com>
2021-05-17 00:25:05 +0300

    Required functions are added to Calendar Event page

--- frontend/src/app/pages/page-calendar-event/page-calendar-event.page.ts
+++ frontend/src/app/pages/page-calendar-event/page-calendar-event.page.ts
@@ -1,6 +1,14 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {CalendarEvent} from '../../providers/model/calendar-event/calendar-event.type';
+import {environment} from '../../../environments/environment';
 
+interface City{
+  name: string;
+}
 @Component({
   selector: 'app-page-calendar-event',
   templateUrl: './page-calendar-event.page.html',
@@ -12,18 +20,291 @@ export class PageCalendarEventPage implements AfterViewInit {
     currentDate: new Date()
   };
   viewTitle: string;
-
-  event = {
-    title: '',
-    desc: '',
-    startTime: null,
-    endTime: '',
-    allDay: true
-  };
+  ionicForm: FormGroup;
+  submitted;
 
   modalReady = false;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(public formBuilder: FormBuilder, private modalCtrl: ModalController, private router: Router, private http: HttpClient) {
+    this.ionicForm = formBuilder.group({
+      city: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      meetingPlace: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      meetingUrl: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      startTime: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+      endTime: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])
+      ],
+    });
+  }
+
+  cities: City[] = [
+    {
+      name: 'Adana',
+    },
+    {
+      name: 'Adıyaman',
+    },
+    {
+      name: 'Afyonkarahisar',
+    },
+    {
+      name: 'Ağrı',
+    },
+    {
+      name: 'Aksaray',
+    },
+    {
+      name: 'Amasya',
+    },
+    {
+      name: 'Ankara',
+    },
+    {
+      name: 'Antalya',
+    },
+    {
+      name: 'Ardahan',
+    },
+    {
+      name: 'Artvin',
+    },
+    {
+      name: 'Aydın',
+    },
+    {
+      name: 'Balıkesir',
+    },
+    {
+      name: 'Bartın',
+    },
+    {
+      name: 'Batman',
+    },
+    {
+      name: 'Bayburt',
+    },
+    {
+      name: 'Bilecik',
+    },
+    {
+      name: 'Bingöl',
+    },
+    {
+      name: 'Bitlis',
+    },
+    {
+      name: 'Bolu',
+    },
+    {
+      name: 'Burdur',
+    },
+    {
+      name: 'Bursa',
+    },
+    {
+      name: 'Çanakkale',
+    },
+    {
+      name: 'Çankırı',
+    },
+    {
+      name: 'Çorum',
+    },
+    {
+      name: 'Denizli',
+    },
+    {
+      name: 'Diyarbakır',
+    },
+    {
+      name: 'Düzce',
+    },
+    {
+      name: 'Edirne',
+    },
+    {
+      name: 'Elazığ',
+    },
+    {
+      name: 'Erzincan',
+    },
+    {
+      name: 'Erzurum',
+    },
+    {
+      name: 'Eskişehir',
+    },
+    {
+      name: 'Gaziantep',
+    },
+    {
+      name: 'Giresun',
+    },
+    {
+      name: 'Gümüşhane',
+    },
+    {
+      name: 'Hakkari',
+    },
+    {
+      name: 'Hatay',
+    },
+    {
+      name: 'Iğdır',
+    },
+    {
+      name: 'Isparta',
+    },
+    {
+      name: 'İstanbul',
+    },
+    {
+      name: 'İzmir',
+    },
+    {
+      name: 'Kahramanmaraş',
+    },
+    {
+      name: 'Karabük',
+    },
+    {
+      name: 'Karaman',
+    },
+    {
+      name: 'Kars',
+    },
+    {
+      name: 'Kastamonu',
+    },
+    {
+      name: 'Kayseri',
+    },
+    {
+      name: 'Kırıkkale',
+    },
+    {
+      name: 'Kırklareli',
+    },
+    {
+      name: 'Kırşehir',
+    },
+    {
+      name: 'Kilis',
+    },
+    {
+      name: 'Kocaeli',
+    },
+    {
+      name: 'Konya',
+    },
+    {
+      name: 'Kütahya',
+    },
+    {
+      name: 'Malatya',
+    },
+    {
+      name: 'Manisa',
+    },
+    {
+      name: 'Mardin',
+    },
+    {
+      name: 'Mersin',
+    },
+    {
+      name: 'Muğla',
+    },
+    {
+      name: 'Muş',
+    },
+    {
+      name: 'Nevşehir',
+    },
+    {
+      name: 'Niğde',
+    },
+    {
+      name: 'Ordu',
+    },
+    {
+      name: 'Osmaniye',
+    },
+    {
+      name: 'Rize',
+    },
+    {
+      name: 'Sakarya',
+    },
+    {
+      name: 'Samsun',
+    },
+    {
+      name: 'Siirt',
+    },
+    {
+      name: 'Sinop',
+    },
+    {
+      name: 'Sivas',
+    },
+    {
+      name: 'Şanlıurfa',
+    },
+    {
+      name: 'Şırnak',
+    },
+    {
+      name: 'Tekirdağ',
+    },
+    {
+      name: 'Tokat',
+    },
+    {
+      name: 'Trabzon',
+    },
+    {
+      name: 'Tunceli',
+    },
+    {
+      name: 'Uşak',
+    },
+    {
+      name: 'Van',
+    },
+    {
+      name: 'Yalova',
+    },
+    {
+      name: 'Yozgat',
+    },
+    {
+      name: 'Zonguldak',
+    }
+  ];
 
   ngAfterViewInit() {
     setTimeout(() => {
@@ -31,8 +312,29 @@ export class PageCalendarEventPage implements AfterViewInit {
     }, 0);
   }
 
-  save() {
-    this.modalCtrl.dismiss({event: this.event});
+  async saveEvent() {
+    const eventData: CalendarEvent = {...this.ionicForm.value};
+    eventData.city = (eventData.city as any).name;
+
+    this.submitted = true;
+    if (this.ionicForm.invalid) {
+      console.log('All fields are required.');
+      this.router.navigate(['/feed']);
+      return false;
+    }else{
+      console.log(eventData);
+      try{
+        await this.http.post(environment.apiUrl + '/api/', eventData).toPromise();
+        this.router.navigate(['/feed']);
+      } catch (e){
+        // error
+      }
+    }
+    // this.modalCtrl.dismiss({event: this.event});
+  }
+
+  get errorCtr() {
+    return this.ionicForm.controls;
   }
 
   onViewTitleChanged(title) {
@@ -40,7 +342,7 @@ export class PageCalendarEventPage implements AfterViewInit {
   }
 
   onTimeSelected(ev) {
-    this.event.startTime = new Date(ev.selectedTime);
+    // this.event.startTime = new Date(ev.selectedTime);
   }
 
   close() {
