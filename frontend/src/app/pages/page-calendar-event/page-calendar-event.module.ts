4afd5a879f4310542fae255b3c54bde605531514
ranacakmak <rana.cakmak.89@gmail.com>
2021-05-17 00:24:03 +0300

    A new module is imported to Calendar Event page

--- frontend/src/app/pages/page-calendar-event/page-calendar-event.module.ts
+++ frontend/src/app/pages/page-calendar-event/page-calendar-event.module.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -10,13 +10,14 @@ import { PageCalendarEventPage } from './page-calendar-event.page';
 import {NgCalendarModule} from 'ionic2-calendar';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    PageCalendarEventPageRoutingModule,
-    NgCalendarModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        PageCalendarEventPageRoutingModule,
+        NgCalendarModule,
+        ReactiveFormsModule,
+    ],
   declarations: [PageCalendarEventPage]
 })
 export class PageCalendarEventPageModule{}
