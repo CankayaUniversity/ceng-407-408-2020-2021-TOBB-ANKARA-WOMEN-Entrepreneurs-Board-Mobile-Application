6b323f1b1e5ae9afbabbadd21c703423a557c288
ranacakmak <rana.cakmak.89@gmail.com>
2021-05-20 13:48:29 +0300

    New modules are imported to the Search page

--- frontend/src/app/pages/page-search/page-search.module.ts
+++ frontend/src/app/pages/page-search/page-search.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { PageSearchPageRoutingModule } from './page-search-routing.module';
-
 import { PageSearchPage } from './page-search.page';
 
 @NgModule({
@@ -13,6 +11,7 @@ import { PageSearchPage } from './page-search.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    Ng2SearchPipeModule,
     PageSearchPageRoutingModule
   ],
   declarations: [PageSearchPage]
--- frontend/src/app/pages/page-search/page-search.page.ts
+++ frontend/src/app/pages/page-search/page-search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../providers/model/user/user.model';
+import { UsersService } from 'src/app/providers/service/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-search',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageSearchPage implements OnInit {
 
-  constructor() { }
+  filterTerm: string;
+  user: User;
+  userData: any;
+
+  constructor(private usersService: UsersService,
+              private router: Router,
+  ) {
+  }
 
   ngOnInit() {
+    this.usersService.getUsers('/api/user')
+      .subscribe(data => {
+        console.log(data);
+        this.userData = data;
+      });
   }
 
 }
+
